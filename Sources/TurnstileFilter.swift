37ae168e8dcf82c17e58d749d7961acf53e8ce6c
Edward Jiang <edward@stormpath.com>
2016-08-31 09:38:56 -0700

    adding response filter to store session

---
+++ Sources/TurnstileFilter.swift
@@ -0,0 +1,60 @@
+//
+//  TurnstileRequestFilter.swift
+//  TurnstilePerfect
+//
+//  Created by Edward Jiang on 8/23/16.
+//
+//
+
+import PerfectHTTP
+import Turnstile
+
+public class TurnstileFilter {
+    fileprivate let turnstile: Turnstile
+    
+    init(turnstile: Turnstile) {
+        self.turnstile = turnstile
+    }
+}
+
+extension TurnstileFilter: HTTPRequestFilter {
+
+    public func filter(request: HTTPRequest, response: HTTPResponse, callback: (HTTPRequestFilterResult) -> ()) {
+        // Initialize session
+        // Token/API Key Auth
+        //
+        request.user = Subject(turnstile: turnstile, sessionID: request.getCookie(name: "TurnstileSession"))
+        
+        print("Cookie: \(request.getCookie(name: "TurnstileSession"))")
+        
+        if let apiKeys = request.auth?.basic {
+            try? request.user.login(credentials: apiKeys)
+        } else if let token = request.auth?.bearer {
+            try? request.user.login(credentials: token)
+        }
+        
+        callback(HTTPRequestFilterResult.continue(request, response))
+    }
+}
+
+extension TurnstileFilter: HTTPResponseFilter {
+
+    /// Called once before headers are sent to the client.
+    public func filterHeaders(response: HTTPResponse, callback: (HTTPResponseFilterResult) -> ()) {
+        if let sessionID = response.request.user.authDetails?.sessionID {
+            response.addCookie(HTTPCookie(name: "TurnstileSession",
+                                          value: "\(sessionID)",
+                domain: nil,
+                expires: .relativeSeconds(60*60*24*365),
+                path: "/",
+                secure: nil,
+                httpOnly: true))
+        }
+        callback(.continue)
+    }
+    
+    /// Called zero or more times for each bit of body data which is sent to the client.
+    public func filterBody(response: HTTPResponse, callback: (HTTPResponseFilterResult) -> ()) {
+        callback(.continue)
+    }
+}
--- Sources/TurnstilePerfect.swift
+++ Sources/TurnstilePerfect.swift
@@ -2,14 +2,13 @@ import Turnstile
 import PerfectHTTP
 
 public class TurnstilePerfect {
-    public var requestFilter: (HTTPRequestFilter, HTTPFilterPriority) {
-        return (TurnstileRequestFilter(turnstile: turnstile), HTTPFilterPriority.high)
-    }
+    public var filter: (HTTPRequestFilter, HTTPFilterPriority)
     
     private let turnstile: Turnstile
     
     public init(sessionManager: SessionManager = MemorySessionManager(), realm: Realm = MemoryRealm()) {
         turnstile = Turnstile(sessionManager: MemorySessionManager(), realm: MemoryRealm())
+        filter = (TurnstileFilter(turnstile: turnstile), HTTPFilterPriority.high)
     }
 }
 
