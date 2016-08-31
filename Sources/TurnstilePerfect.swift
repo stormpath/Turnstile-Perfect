e600306571cdff667273b84d8b965011b9353044
Edward Jiang <edward@stormpath.com>
2016-08-31 09:46:48 -0700

    compiler doesn't like casting tuples, so second best solution here

--- Sources/TurnstilePerfect.swift
+++ Sources/TurnstilePerfect.swift
@@ -2,13 +2,19 @@ import Turnstile
 import PerfectHTTP
 
 public class TurnstilePerfect {
-    public var filter: (HTTPRequestFilter, HTTPFilterPriority)
+    public var requestFilter: (HTTPRequestFilter, HTTPFilterPriority)
+    public var responseFilter: (HTTPResponseFilter, HTTPFilterPriority)
     
     private let turnstile: Turnstile
     
     public init(sessionManager: SessionManager = MemorySessionManager(), realm: Realm = MemoryRealm()) {
         turnstile = Turnstile(sessionManager: MemorySessionManager(), realm: MemoryRealm())
-        filter = (TurnstileFilter(turnstile: turnstile), HTTPFilterPriority.high)
+        let filter = TurnstileFilter(turnstile: turnstile)
+        
+        // Not sure how polymorphicism works with tuples, but the compiler was crashing on me
+        // So I did this
+        requestFilter = (filter, HTTPFilterPriority.high)
+        responseFilter = (filter, HTTPFilterPriority.high)
     }
 }
 
